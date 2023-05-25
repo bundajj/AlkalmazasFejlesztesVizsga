977b5139eea0a688c56e1d4c86a8f67d8f09c9d5
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-25 18:45:28 +0200

    ID alapján Sponsor változtató REST végpont

--- src/main/java/hu/gde/runnersdemo/RunnerRestController.java
+++ src/main/java/hu/gde/runnersdemo/RunnerRestController.java
@@ -14,11 +14,13 @@ public class RunnerRestController {
     @Autowired
     private LapTimeRepository lapTimeRepository;
     private RunnerRepository runnerRepository;
+    private SponsorRepository sponsorRepository;
 
     @Autowired
-    public RunnerRestController(RunnerRepository runnerRepository, LapTimeRepository lapTimeRepository) {
+    public RunnerRestController(RunnerRepository runnerRepository, LapTimeRepository lapTimeRepository,SponsorRepository sponsorRepository) {
         this.runnerRepository = runnerRepository;
         this.lapTimeRepository = lapTimeRepository;
+        this.sponsorRepository=sponsorRepository;
     }
 
     @GetMapping("/{id}")
@@ -105,6 +107,19 @@ public class RunnerRestController {
             return -1.0;
         }
     }
+    @PostMapping("/{id}/setsponsor")
+    public ResponseEntity setSponsor(@PathVariable Long id, @RequestBody sponsorRequest sponsorRequest) {
+        RunnerEntity runner = runnerRepository.findById(id).orElse(null);
+        SponsorEntity sponsor = sponsorRepository.findById(sponsorRequest.getSponsorId()).orElse(null);
+        if(runner != null && sponsor != null) {
+            runner.setSponsor(sponsor);
+            runnerRepository.save(runner);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Runner with ID " + id + " not found");
+        }
+    }
+
     public static class LapTimeRequest {
         private int lapTimeSeconds;
 
@@ -114,6 +129,17 @@ public class RunnerRestController {
 
         public void setLapTimeSeconds(int lapTimeSeconds) {
             this.lapTimeSeconds = lapTimeSeconds;
+        }
+    }
+    public static class sponsorRequest {
+        private long sponsorId;
+
+        public long getSponsorId() {
+            return sponsorId;
+        }
+
+        public void setSponsorId(long sponsorId) {
+            this.sponsorId = sponsorId;
         }
     }
 }
